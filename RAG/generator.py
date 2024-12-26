20c3a48088f3ef70f206702387ec55115757e188
Coder-C18 <cao2000nt@gmail.com>
2024-12-26 23:04:00 +0700

    update generator v4

--- RAG/generator.py
+++ RAG/generator.py
@@ -12,30 +12,12 @@ from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.messages import HumanMessage, AIMessage
 from redisvl.extensions.llmcache import SemanticCache
-
+from PROMPT import *
 logging.basicConfig()
 logging.getLogger("langchain.retrievers.multi_query").setLevel(logging.INFO)
 
 
-MUTIL_QUERY_PROMPT = """
-Bạn là một trợ lý mô hình ngôn ngữ AI. Nhiệm vụ của bạn là tạo ra năm phiên bản khác nhau 
-của câu hỏi người dùng đã cho để truy xuất các tài liệu liên quan từ cơ sở dữ liệu vector. 
-Bằng cách tạo ra nhiều góc nhìn khác nhau về câu hỏi của người dùng, mục tiêu của bạn là 
-giúp người dùng vượt qua một số hạn chế của việc tìm kiếm dựa trên sự tương đồng theo khoảng 
-cách.Cung cấp những câu hỏi thay thế này, mỗi câu cách nhau bằng một dòng mới.
-Câu hỏi gốc: {question}"""
 
-CONTEXTUALIZE_PROMPT = """
-Dựa trên lịch sử trò chuyện và câu hỏi người dùng mới nhất có thể tham chiếu đến ngữ cảnh trong 
-lịch sử trò chuyện, hãy xây dựng lại câu hỏi độc lập sao cho có thể hiểu được mà không cần đến 
-lịch sử trò chuyện. ĐỪNG trả lời câu hỏi, chỉ cần tái cấu trúc câu hỏi nếu cần và nếu không cần 
-thay đổi thì trả lại câu hỏi nguyên bản."""
-QA_SYSTEM_PROMPT = """
-Bạn Là Elsa , một nữ nhân viên tư vấn thông minh , có tài ăn nói và  có nhiều năm kinh nghiệm trong 
-việc tư vấn và hỗ trợ . Hãy sử dụng các đoạn ngữ cảnh đã được truy xuất dưới 
-đây để trả lời câu hỏi. Nếu đoạn ngữ cảnh không có thông tin liên quan , chỉ cần nói rằng bạn không biết. Hãy giữ câu trả lời
- ngắn gọn, tự nhiên  và không quá 10 câu.
-{context}"""
 vector_store = QdrantVectorStore(
     client=client_db,
     collection_name="Nội quy công ty GEM 2.docx",
@@ -120,10 +102,7 @@ class RAGChain:
         )
         self.cache.clear()
 
-        self.rewrite_prompt = PromptTemplate(template="""Bạn là AI thông minh và tận tâm. Nhiệm vụ của bạn là viết lại câu trả lời sao cho phù hợp với câu hỏi, giữ nguyên ý nghĩa cốt lõi nhưng đáp ứng đúng mục đích của câu hỏi. 
-                                                Sau khi viết lại, tự đánh giá và chỉnh sửa đến khi phù hợp. Trả về duy nhất câu trả lời, không quá 5 câu.
-                                                Câu hỏi: {query}
-                                                Câu trả lời: {response}""",
+        self.rewrite_prompt = PromptTemplate(template=REWRITE_PROMPT,
                                              input_variables=["query", "response"]
                                              )
 
